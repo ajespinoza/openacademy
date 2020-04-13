c36c80c720ece3a3b463d2cd339f1e8ca32e5bbf
ajespinoza <angelo_19_94@hotmail.com>
2020-04-12 23:57:07 -0500

    agrego tab

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
-    _name = 'openacademy.course'
-    _description = "OpenAcademy Courses"
+	_name = 'openacademy.course'
+	_description = "OpenAcademy Courses"
 
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+	name = fields.Char(string="Title", required=True)
+	description = fields.Text()
 
 	responsible_id = fields.Many2one('res.users',
 		ondelete='set null', string="Responsible", index=True)
@@ -14,17 +14,17 @@ class Course(models.Model):
 
 
 class Session(models.Model):
-    _name = 'openacademy.session'
-    _description = "OpenAcademy Sessions"
+	_name = 'openacademy.session'
+	_description = "OpenAcademy Sessions"
 
-    name = fields.Char(required=True)
-    start_date = fields.Date()
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
-    seats = fields.Integer(string="Number of seats")
+	name = fields.Char(required=True)
+	start_date = fields.Date()
+	duration = fields.Float(digits=(6, 2), help="Duration in days")
+	seats = fields.Integer(string="Number of seats")
 
 	instructor_id = fields.Many2one('res.partner', string="Instructor",
-        domain=['|', ('instructor', '=', True),
-                     ('category_id.name', 'ilike', "Teacher")])
-    course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
+		domain=['|', ('instructor', '=', True),
+			('category_id.name', 'ilike', "Teacher")])
+	course_id = fields.Many2one('openacademy.course',
+		ondelete='cascade', string="Course", required=True)
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
