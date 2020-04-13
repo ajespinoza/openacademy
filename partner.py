8945f0933b5c3ade7b9a2eaecc716f942b3afe8b
ajespinoza <angelo_19_94@hotmail.com>
2020-04-13 13:49:43 -0500

    correccion tabs

--- partner.py
+++ partner.py
@@ -7,6 +7,5 @@ class Partner(models.Model):
 	# Add a new column to the res.partner model, by default partners are not
 	# instructors
 	instructor = fields.Boolean("Instructor", default=False)
-
 	session_ids = fields.Many2many('openacademy.session',
 		string="Attended Sessions", readonly=True)
