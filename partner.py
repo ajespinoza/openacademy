8fd13b73aafaa13873251550789b353bd4deea7f
ajespinoza <angelo_19_94@hotmail.com>
2020-04-13 13:52:05 -0500

    correccion tabs

--- partner.py
+++ partner.py
@@ -2,7 +2,7 @@
 from odoo import fields, models
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+	_inherit = 'res.partner'
 
 	# Add a new column to the res.partner model, by default partners are not
 	# instructors
